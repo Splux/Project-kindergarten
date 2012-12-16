92946df9580d8a17e4085f941904f9eaa81d6f9b
Splux <MrSplux@gmail.com>
2012-12-16 23:59:04 +0100

    Pressing "Register" will bring out everything register-related;
    Pressing "Close" will close everything related to registerform;
    Renamed labels to more readable and understandable names;
    
    Known bug:
    It's a bit glitchy when pressing "Register", will fix whenever I know how...

--- loginmenu.Designer.cs
+++ loginmenu.Designer.cs
@@ -33,17 +33,19 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.btn_Login = new System.Windows.Forms.Button();
-            this.btn_Register = new System.Windows.Forms.Button();
+            this.btn_Register1 = new System.Windows.Forms.Button();
             this.textBox_ConfirmPw = new System.Windows.Forms.TextBox();
             this.textBox_RegPw = new System.Windows.Forms.TextBox();
             this.textBox_RegUsrname = new System.Windows.Forms.TextBox();
-            this.label3 = new System.Windows.Forms.Label();
-            this.labelX = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
+            this.lbl_RegEmail = new System.Windows.Forms.Label();
+            this.lbl_RegPw = new System.Windows.Forms.Label();
+            this.lbl_RegUser = new System.Windows.Forms.Label();
             this.textBox_LoginEmail = new System.Windows.Forms.TextBox();
-            this.label4 = new System.Windows.Forms.Label();
+            this.lbl_RegConfPw = new System.Windows.Forms.Label();
             this.pb_Border = new System.Windows.Forms.PictureBox();
             this.btn_Exit = new System.Windows.Forms.Button();
+            this.btn_Register = new System.Windows.Forms.Button();
+            this.btn_Close = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pb_Border)).BeginInit();
             this.SuspendLayout();
             // 
@@ -82,23 +84,23 @@
             // 
             // btn_Login
             // 
-            this.btn_Login.Location = new System.Drawing.Point(104, 119);
+            this.btn_Login.Location = new System.Drawing.Point(12, 114);
             this.btn_Login.Name = "btn_Login";
-            this.btn_Login.Size = new System.Drawing.Size(75, 41);
+            this.btn_Login.Size = new System.Drawing.Size(107, 41);
             this.btn_Login.TabIndex = 4;
             this.btn_Login.Text = "Login!";
             this.btn_Login.UseVisualStyleBackColor = true;
             this.btn_Login.Click += new System.EventHandler(this.btn_Login_Click);
             // 
-            // btn_Register
+            // btn_Register1
             // 
-            this.btn_Register.Location = new System.Drawing.Point(493, 231);
-            this.btn_Register.Name = "btn_Register";
-            this.btn_Register.Size = new System.Drawing.Size(75, 22);
-            this.btn_Register.TabIndex = 5;
-            this.btn_Register.Text = "Register!";
-            this.btn_Register.UseVisualStyleBackColor = true;
-            this.btn_Register.Click += new System.EventHandler(this.btn_Register_Click);
+            this.btn_Register1.Location = new System.Drawing.Point(405, 202);
+            this.btn_Register1.Name = "btn_Register1";
+            this.btn_Register1.Size = new System.Drawing.Size(139, 38);
+            this.btn_Register1.TabIndex = 5;
+            this.btn_Register1.Text = "Register!";
+            this.btn_Register1.UseVisualStyleBackColor = true;
+            this.btn_Register1.Click += new System.EventHandler(this.btn_Register1_Click);
             // 
             // textBox_ConfirmPw
             // 
@@ -123,32 +125,32 @@
             this.textBox_RegUsrname.Size = new System.Drawing.Size(260, 20);
             this.textBox_RegUsrname.TabIndex = 8;
             // 
-            // label3
+            // lbl_RegEmail
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(512, 160);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(32, 13);
-            this.label3.TabIndex = 9;
-            this.label3.Text = "Email";
+            this.lbl_RegEmail.AutoSize = true;
+            this.lbl_RegEmail.Location = new System.Drawing.Point(512, 160);
+            this.lbl_RegEmail.Name = "lbl_RegEmail";
+            this.lbl_RegEmail.Size = new System.Drawing.Size(32, 13);
+            this.lbl_RegEmail.TabIndex = 9;
+            this.lbl_RegEmail.Text = "Email";
             // 
-            // labelX
+            // lbl_RegPw
             // 
-            this.labelX.AutoSize = true;
-            this.labelX.Location = new System.Drawing.Point(512, 62);
-            this.labelX.Name = "labelX";
-            this.labelX.Size = new System.Drawing.Size(53, 13);
-            this.labelX.TabIndex = 10;
-            this.labelX.Text = "Password";
+            this.lbl_RegPw.AutoSize = true;
+            this.lbl_RegPw.Location = new System.Drawing.Point(512, 62);
+            this.lbl_RegPw.Name = "lbl_RegPw";
+            this.lbl_RegPw.Size = new System.Drawing.Size(53, 13);
+            this.lbl_RegPw.TabIndex = 10;
+            this.lbl_RegPw.Text = "Password";
             // 
-            // label5
+            // lbl_RegUser
             // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(512, 9);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(55, 13);
-            this.label5.TabIndex = 11;
-            this.label5.Text = "Username";
+            this.lbl_RegUser.AutoSize = true;
+            this.lbl_RegUser.Location = new System.Drawing.Point(512, 9);
+            this.lbl_RegUser.Name = "lbl_RegUser";
+            this.lbl_RegUser.Size = new System.Drawing.Size(55, 13);
+            this.lbl_RegUser.TabIndex = 11;
+            this.lbl_RegUser.Text = "Username";
             // 
             // textBox_LoginEmail
             // 
@@ -157,14 +159,14 @@
             this.textBox_LoginEmail.Size = new System.Drawing.Size(260, 20);
             this.textBox_LoginEmail.TabIndex = 12;
             // 
-            // label4
+            // lbl_RegConfPw
             // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(490, 114);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(91, 13);
-            this.label4.TabIndex = 13;
-            this.label4.Text = "Confirm Password";
+            this.lbl_RegConfPw.AutoSize = true;
+            this.lbl_RegConfPw.Location = new System.Drawing.Point(490, 114);
+            this.lbl_RegConfPw.Name = "lbl_RegConfPw";
+            this.lbl_RegConfPw.Size = new System.Drawing.Size(91, 13);
+            this.lbl_RegConfPw.TabIndex = 13;
+            this.lbl_RegConfPw.Text = "Confirm Password";
             // 
             // pb_Border
             // 
@@ -185,22 +187,44 @@
             this.btn_Exit.UseVisualStyleBackColor = true;
             this.btn_Exit.Click += new System.EventHandler(this.btn_Exit_Click);
             // 
+            // btn_Register
+            // 
+            this.btn_Register.Location = new System.Drawing.Point(164, 114);
+            this.btn_Register.Name = "btn_Register";
+            this.btn_Register.Size = new System.Drawing.Size(108, 41);
+            this.btn_Register.TabIndex = 16;
+            this.btn_Register.Text = "Register";
+            this.btn_Register.UseVisualStyleBackColor = true;
+            this.btn_Register.Click += new System.EventHandler(this.btn_Register_Click);
+            // 
+            // btn_Close
+            // 
+            this.btn_Close.Location = new System.Drawing.Point(543, 202);
+            this.btn_Close.Name = "btn_Close";
+            this.btn_Close.Size = new System.Drawing.Size(122, 38);
+            this.btn_Close.TabIndex = 17;
+            this.btn_Close.Text = "Close";
+            this.btn_Close.UseVisualStyleBackColor = true;
+            this.btn_Close.Click += new System.EventHandler(this.btn_Close_Click);
+            // 
             // loginmenu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(677, 265);
+            this.Controls.Add(this.btn_Close);
+            this.Controls.Add(this.btn_Register);
             this.Controls.Add(this.btn_Exit);
             this.Controls.Add(this.pb_Border);
-            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lbl_RegConfPw);
             this.Controls.Add(this.textBox_LoginEmail);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.labelX);
-            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lbl_RegUser);
+            this.Controls.Add(this.lbl_RegPw);
+            this.Controls.Add(this.lbl_RegEmail);
             this.Controls.Add(this.textBox_RegUsrname);
             this.Controls.Add(this.textBox_RegPw);
             this.Controls.Add(this.textBox_ConfirmPw);
-            this.Controls.Add(this.btn_Register);
+            this.Controls.Add(this.btn_Register1);
             this.Controls.Add(this.btn_Login);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
@@ -210,6 +234,7 @@
             this.Name = "loginmenu";
             this.Text = "Project-Kindergarten v0.01";
             this.Load += new System.EventHandler(this.loginmenu_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.loginmenuKeyDown);
             ((System.ComponentModel.ISupportInitialize)(this.pb_Border)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -223,16 +248,18 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button btn_Login;
-        private System.Windows.Forms.Button btn_Register;
+        private System.Windows.Forms.Button btn_Register1;
         private System.Windows.Forms.TextBox textBox_ConfirmPw;
         private System.Windows.Forms.TextBox textBox_RegPw;
         private System.Windows.Forms.TextBox textBox_RegUsrname;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label labelX;
-        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lbl_RegEmail;
+        private System.Windows.Forms.Label lbl_RegPw;
+        private System.Windows.Forms.Label lbl_RegUser;
         private System.Windows.Forms.TextBox textBox_LoginEmail;
-        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lbl_RegConfPw;
         private System.Windows.Forms.PictureBox pb_Border;
         private System.Windows.Forms.Button btn_Exit;
+        private System.Windows.Forms.Button btn_Register;
+        private System.Windows.Forms.Button btn_Close;
     }
 }
--- loginmenu.cs
+++ loginmenu.cs
@@ -20,13 +20,31 @@ namespace Project_kindergarten
         private void loginmenu_Load(object sender, EventArgs e)
         {
             //byte[] ipaddr = System.Text.ASCIIEncoding.ASCII.GetBytes("172.20.0.123");
-            serverConnection = new TCPConnection(System.Net.IPAddress.Parse("172.20.0.123"));
+            //serverConnection = new TCPConnection(System.Net.IPAddress.Parse("172.20.0.123"));
+            serverConnection = new TCPConnection("asdfs");
 
             // Add eventhandlers for window-dragging functionality
             this.MouseDown += new MouseEventHandler(onMouseDown);
             this.MouseUp += new MouseEventHandler(onMouseUp);
             this.MouseMove += new MouseEventHandler(onMouseMove);
             _dragging = false;
+
+            // Set visibility of register menu to false.
+            textBox_RegUsrname.Visible = false;
+            textBox_RegPw.Visible = false;
+            textBox_ConfirmPw.Visible = false;
+            textBox_LoginEmail.Visible = false;
+
+            btn_Register1.Visible = false;
+
+            lbl_RegConfPw.Visible = false;
+            lbl_RegEmail.Visible = false;
+            lbl_RegPw.Visible = false;
+            lbl_RegUser.Visible = false;
+
+            pb_Border.Visible = false;
+
+            this.Size = new Size(330, 265);
         }
 
 
@@ -69,6 +87,8 @@ namespace Project_kindergarten
 
         private void btn_Login_Click(object sender, EventArgs e)
         {
+            // Keeping this messagebox, in case I need to disable all this again
+            //MessageBox.Show("This section is commented out for testing purpose");
             if (textBox_Username.Text == "" || textBox_Password.Text == "")
             {
                 System.Windows.Forms.MessageBox.Show("Enter both username and/or password");
@@ -90,7 +110,7 @@ namespace Project_kindergarten
             }
         }
 
-        private void btn_Register_Click(object sender, EventArgs e)
+        private void btn_Register1_Click(object sender, EventArgs e)
         {
             if (!textBox_ConfirmPw.Text.Equals(textBox_RegPw.Text))
             {
@@ -103,6 +123,74 @@ namespace Project_kindergarten
         private void btn_Exit_Click(object sender, EventArgs e)
         {
             Application.Exit();
+        }
+
+        private void showRegObjects()
+        {
+            // Set visibility of register menu to true
+            // Textboxes
+            textBox_RegUsrname.Visible = true;
+            textBox_RegPw.Visible = true;
+            textBox_ConfirmPw.Visible = true;
+            textBox_LoginEmail.Visible = true;
+            // Button
+            btn_Register1.Visible = true;
+            // Labels
+            lbl_RegConfPw.Visible = true;
+            lbl_RegEmail.Visible = true;
+            lbl_RegPw.Visible = true;
+            lbl_RegUser.Visible = true;
+            // Border
+            pb_Border.Visible = true;
+            //pb_Border.Update();
+
+            // Set size so we can see it again
+            for (int i = this.Size.Width; i <= 677; i += 3)
+            {
+                this.Size = new Size(i, this.Size.Height);
+            }
+        }
+
+        private void btn_Register_Click(object sender, EventArgs e)
+        {
+            // do it in a thread to avoid a bug I can't solve.
+            System.Threading.Thread th = new System.Threading.Thread(showRegObjects);
+            th.Start();
+            
+        }
+
+        private void btn_Close_Click(object sender, EventArgs e)
+        {
+            
+
+            // Set size so we cannot see it again.
+            for (int i = this.Size.Width; i >= 330; i -= 3)
+            {
+                Size = new Size(i, this.Size.Height);
+            }
+
+            // Set visibility of register menu to false.
+            textBox_RegUsrname.Visible = false;
+            textBox_RegPw.Visible = false;
+            textBox_ConfirmPw.Visible = false;
+            textBox_LoginEmail.Visible = false;
+
+            btn_Register1.Visible = false;
+
+            lbl_RegConfPw.Visible = false;
+            lbl_RegEmail.Visible = false;
+            lbl_RegPw.Visible = false;
+            lbl_RegUser.Visible = false;
+
+            pb_Border.Visible = false;
+        }
+
+        private void loginmenuKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape)
+            {
+                Application.Exit();
+            }
         }
 
         
