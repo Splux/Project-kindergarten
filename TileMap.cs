812ced200df4dc6034ca2a57607f7c044ccef9f2
textur3 <waasd1337@hotmail.com>
2013-04-10 08:59:10 +0200

    started on the tilemap

---
+++ TileMap.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Project_kindergarten
+{
+    class TileMap
+    {
+        TileMap()
+        {
+ 
+        }
+    }
+}
