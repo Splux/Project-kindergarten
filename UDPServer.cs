848edca9e9ce07d79070816d35cd59dc62329f71
wolfart <wolfart_@hotmail.com>
2013-04-26 13:38:52 +0200

    Pull commit

--- UDPServer.cs
+++ UDPServer.cs
@@ -8,61 +8,70 @@ using System.IO;
 using System.Linq;
 using System.Text;
 
-public class UDP
+namespace Project_Kindergarten
 {
-    private UdpClient udp_socket;
-    private bool ServerIsOnline;
-    private Thread BroadcastThread;
-    private List<IPAddress> adresslist = new List<IPAddress>();
-    private IPEndPoint RemoteIpEndPoint;
-    private IPEndPoint SendIpEndPoint;
-    private int PortToSendTo;
-
-    public UDP(IPAddress _IP, int _port, List<IPAddress> _adresslist)
+    public class UDP
     {
-        adresslist = _adresslist;
-        RemoteIpEndPoint = new IPEndPoint(_IP, _port);
-        PortToSendTo = _port;
-    }
+        private UdpClient udp_socket;
+        private volatile bool ServerIsOnline;
+        private Thread BroadcastThread;
+        private List<IPAddress> adresslist = new List<IPAddress>();
+        private IPEndPoint RemoteIpEndPoint;
+        private IPEndPoint SendIpEndPoint;
+        private int PortToSendTo;
 
-    public void StartServer()
-    {
-        udp_socket = new UdpClient();
-        udp_socket.Connect(RemoteIpEndPoint);
-        udp_socket.AllowNatTraversal(true);
-        ServerIsOnline = true;
-
-        BroadcastThread = new Thread(ReceiveData);
-        BroadcastThread.IsBackground = true;
-        BroadcastThread.Start();
-
-    }
-
-    public void ReceiveData()
-    {
-        byte[] data = new byte[11000];
-
-        while (ServerIsOnline == true)
+        public UDP(IPAddress _IP, int _port, List<IPAddress> _adresslist)
         {
-            data = udp_socket.Receive(ref RemoteIpEndPoint);
+            adresslist = _adresslist;
+            RemoteIpEndPoint = new IPEndPoint(_IP, _port);
+            PortToSendTo = _port;
         }
-    }
 
-    public void BroadcastData(String SendData)
-    {
-        Byte[] sendByte = Encoding.ASCII.GetBytes(SendData);
-        foreach (IPAddress adress in adresslist)
+        public void StopServer()
         {
-            try
+            ServerIsOnline = false;
+            BroadcastThread.Abort();
+        }
+
+        public void StartServer()
+        {
+            udp_socket = new UdpClient();
+            udp_socket.Connect(RemoteIpEndPoint);
+            udp_socket.AllowNatTraversal(true);
+            ServerIsOnline = true;
+
+            BroadcastThread = new Thread(ReceiveData);
+            BroadcastThread.IsBackground = true;
+            BroadcastThread.Start();
+
+        }
+
+        public void ReceiveData()
+        {
+            byte[] data = new byte[11000];
+
+            while (ServerIsOnline == true)
             {
-                SendIpEndPoint = new IPEndPoint(adress, PortToSendTo);
-                udp_socket.Connect(SendIpEndPoint);
-                udp_socket.Send(sendByte, sendByte.Length);
-                udp_socket.Close();
+                data = udp_socket.Receive(ref RemoteIpEndPoint);
+                BroadcastData(data);
             }
-            catch
+        }
+
+        public void BroadcastData(Byte[] SendData)
+        {
+            foreach (IPAddress adress in adresslist)
             {
-                System.Windows.Forms.MessageBox.Show("Failed to send data to " + adress.ToString() + ".");
+                try
+                {
+                    SendIpEndPoint = new IPEndPoint(adress, PortToSendTo);
+                    udp_socket.Connect(SendIpEndPoint);
+                    udp_socket.Send(SendData, SendData.Length);
+                    udp_socket.Close();
+                }
+                catch (Exception e)
+                {
+                    System.Windows.Forms.MessageBox.Show("Failed to send data to " + adress.ToString() + ".");
+                }
             }
         }
     }
